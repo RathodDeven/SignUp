11f1109b6dd34abec3aad64a3dd82703d2023ad3
Rathod Deven <devenrathodrd@gmail.com>
2024-08-07 17:43:42 +0530

    add: init repo with custom starter pack

---
+++ components/wrappers/RainbowKitWrapper.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import React from 'react'
+
+import '@rainbow-me/rainbowkit/styles.css'
+
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { WagmiProvider } from 'wagmi'
+import { mainnet, polygon, optimism, arbitrum, base, zora } from 'wagmi/chains'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, polygon, optimism, arbitrum, base, zora],
+  ssr: true // If your dApp uses server side rendering (SSR)
+})
+
+const queryClient = new QueryClient()
+
+const RainbowKitWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default RainbowKitWrapper
